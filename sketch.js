958dc8096df86d112beea35a33482b9596c77232
DNAgb <43895611+DNAgb@users.noreply.github.com>
2019-11-26 14:06:35 -0800

    game setup/psudocode

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+// declare variables for:
+// game state
+// hitboxs for walls
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  //   if game state variable is "title"
+  //   then draw the title page
+  // (execute title page function)
+
+  //   if button pressed game state is "first level"
+  //   execute first level function
+
+  // if walls are hit(maybe wheneveer wall his hit dicrease user score-maybe given randomly at the begginging(commentary on life))
+  // execute game over function
+
+
+}
+
+function displayTitlePage(){
+
+  // make background black
+  // display image of my sprite
+  // display text "claustrophobia"
+  // have button (play) under title
+
+}
+function firstLevel(){
+  //load first level labarynth style but walls move closer the more you move
+
+  //movement attached to mouse position
+  //have a hitbox for the box and have a hitbox for the movving walls as well as the various walls of the labaryinth
+
+  //attach variable to pizels moved
+
+  //use that variable and multiply it to the size of the two plck boxes that are spaned on the edges of the screeen(continueous update)
+
+}
